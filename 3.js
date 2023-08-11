cdddceaedee9b40fd24ca002a3ac8151e7f4b9d4
teddyadji <teddy.adji@cashenable.com>
2023-08-11 19:41:33 +0700

    update

--- 3.js
+++ 3.js
@@ -10,7 +10,7 @@ const time5 = 50
 const time6 = 50
 const time7 = 50
 const time8 = 50
-const total1 = time1 + time2 + time3 + time4
+const total1 = time1 - time2 - time3 + time4
 
 console.log(total)
 
